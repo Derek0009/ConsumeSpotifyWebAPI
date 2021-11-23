361ff6568c0e428fadc35f7d8928ae18785c2ecc
vaghelad <vaghelad@5PSJD22-LPT>
2021-11-23 09:56:32 +0000

    Add project files.

---
+++ ConsumeSpotifyWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ConsumeSpotifyWebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumeSpotifyWebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISpotifyAccountService _spotifyAccountService;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ISpotifyAccountService spotifyAccountService, IConfiguration configuration)
+        {
+            _spotifyAccountService = spotifyAccountService;
+            _configuration = configuration;
+        }
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var token = await _spotifyAccountService.GetToken(_configuration["Spotify:ClientId"], _configuration["Spotify:ClientSecret"]);
+            }
+
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ ConsumeSpotifyWebAPI/Services/SpotifyService.cs
@@ -0,0 +1,35 @@
+﻿using ConsumeSpotifyWebAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsumeSpotifyWebAPI.Services
+{
+    public class SpotifyService : ISpotifyService
+    {
+        private HttpClient _httpClient;
+
+        public SpotifyService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IEnumerable<Release>> GetNewReleases(string countryCode, int limit, string accessToken)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            var response = await _httpClient.GetAsync($"browse/new-releases?country={countryCode}&limit={limit}");
+            response.EnsureSuccessStatusCode();
+
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            var responseObject = await JsonSerializer.DeserializeAsync<ConsumeSpotifyWebAPI.Models.>(responseStream);
+
+            return responseObject?.albums?.items.Select(i => new Release
+            {
+
+            });
+        }   
+    }
+}
